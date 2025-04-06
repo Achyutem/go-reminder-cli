2a8f1cd2e838fd0eef91dcc104b4ff5507fe43f5
achyutem <achyutem.ad@gmail.com>
2025-04-06 13:13:16 +0530

    cleanup

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ const (
 
 func main() {
 	if len(os.Args) < 3 {
-		fmt.Printf("Usage: %s <hh:mm> <Text Message>\n", os.Args[0])
+		fmt.Printf("Usage: %s <time expression> <Text Message>\nExample: %s \"in 10 minutes\" Take a break\n", os.Args[0], os.Args[0])
 		os.Exit(1)
 	}
 
@@ -30,40 +30,34 @@ func main() {
 	w.Add(common.All...)
 
 	t, err := w.Parse(os.Args[1], now)
-	if err != nil {
-		fmt.Println("Error parsing time:", err)
-		os.Exit(2)
-	}
-
-	if t == nil {
-		fmt.Println("Unable to parse time")
+	if err != nil || t == nil {
+		fmt.Println("Could not parse time. Try something like 'in 10 minutes' or '10:30am'")
 		os.Exit(2)
 	}
 
 	if now.After(t.Time) {
-		fmt.Println("Please set a future time")
+		fmt.Println("Time must be in the future.")
 		os.Exit(3)
 	}
 
+	message := strings.Join(os.Args[2:], " ")
+
 	if os.Getenv(markName) == markValue {
-		// This is the reminder execution block
-		err = beeep.Alert("Reminder", strings.Join(os.Args[2:], " "), "assets/information.png")
+		err = beeep.Notify("Reminder", message, "")
 		if err != nil {
-			fmt.Println("Error displaying reminder:", err)
-			os.Exit(4)
+			fmt.Println("Reminder:", message)
 		}
 		os.Exit(0)
 	} else {
-		// Calculate the time difference and schedule the reminder
 		diff := t.Time.Sub(now)
-		fmt.Println("Reminder will be displayed after", diff.Round(time.Second))
+		fmt.Println("Reminder scheduled in", diff.Round(time.Second))
+
 		time.Sleep(diff)
 
-		// Re-run the program with the environment variable set
 		cmd := exec.Command(os.Args[0], os.Args[1:]...)
 		cmd.Env = append(os.Environ(), fmt.Sprintf("%s=%s", markName, markValue))
 		if err := cmd.Start(); err != nil {
-			fmt.Println("Error restarting process:", err)
+			fmt.Println("Error launching reminder:", err)
 			os.Exit(5)
 		}
 		os.Exit(0)
